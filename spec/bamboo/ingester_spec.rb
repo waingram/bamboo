31ea478c5b603031eceea9f5070b9773ca8167c5
Bill Ingram <waingram@gmail.com>
2011-06-16 19:13:50 -0500

    Added test for create book

--- spec/bamboo/ingester_spec.rb
+++ spec/bamboo/ingester_spec.rb
@@ -9,11 +9,11 @@ module Bamboo
     context "finding files" do
 
       before(:each) do
-        #unadorned_path = File.join PROJECT_ROOT,"spec","fixtures","ecco-unadorned"
-        adorned_path    = File.join PROJECT_ROOT, "spec", "fixtures", "ecco-adorned-p5simple"
-        @unadorned_path = "/home/medusa/Downloads/TCPSource/1420ECCOtexts/1420ECCOtexts"
+        @unadorned_path = File.join PROJECT_ROOT,"spec","fixtures","ecco-unadorned"
+        @adorned_path    = File.join PROJECT_ROOT, "spec", "fixtures", "ecco-adorned-p5simple"
+        #unadorned_path = "/home/medusa/Downloads/TCPSource/1420ECCOtexts/1420ECCOtexts"
 
-        @ingester = Bamboo::Ingester.new(@unadorned_path, adorned_path)
+        @ingester = Bamboo::Ingester.new(@unadorned_path, @adorned_path)
       end
 
       after(:each) do
@@ -27,16 +27,26 @@ module Bamboo
           tei_filename = File.basename(f)
           puts "Processing TCP file: #{f}"
           @ingester.load_tei(tei_filename)
-
+          
+          # test image URLs randomly 
           img = @ingester.image_urls[rand(@ingester.image_urls.size)]
           url = URI.parse(img)
           puts "Testing URL: #{url}"
+          
+          # test this is a valid URL, and the content is TIFF
           Net::HTTP.start(url.host, url.port) do |http|
             response = http.get(url.request_uri)
             response.should.kind_of? Net::HTTPOK
             response.content_type.should == "image/tiff"
           end
         end
+      end
+      
+      it "should create a bamboo book" do
+        tei_filename = "K000122.000.xml"
+        @ingester.load_tei(tei_filename)
+        book = @ingester.create_book
+        book.should_not.be_nil
       end
 
     end
